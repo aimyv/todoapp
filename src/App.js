c2448453b98621008b72b4c47b2f5da5d3033071
aimyv <av533@bath.ac.uk>
2023-09-30 15:21:33 +0100

    made title larger

--- src/App.js
+++ src/App.js
@@ -35,12 +35,12 @@ export default function App() {
 
     return (
         <div className='app'>
-          <div>
-            <h1><span id='underline'>To Do</span> or <span id='throughline'>Not</span> To Do?</h1>
-            <br />
-            <SortAndFilter todoList={todoList} setTodoList={setTodoList} isChronological={isChronological} setIsChonological={setIsChonological} setStatus={setStatus}/>
-            <ToDoList filteredTodos={filteredTodos} todoList={todoList} setTodoList={setTodoList} />
-          </div>
+            <div>
+                <h1><span id='underline'>To Do</span> or <span id='throughline'>Not</span> To Do?</h1>
+                <br />
+                <SortAndFilter todoList={todoList} setTodoList={setTodoList} isChronological={isChronological} setIsChonological={setIsChonological} setStatus={setStatus}/>
+                <ToDoList filteredTodos={filteredTodos} todoList={todoList} setTodoList={setTodoList} />
+            </div>
             <ToDoForm todo={todo} setTodo={setTodo} todoList={todoList} setTodoList={setTodoList} />      
         </div>
     )
